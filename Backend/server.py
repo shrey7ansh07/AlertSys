efcb98b74bbdd349d1a2976bc7c42f280f75113c
shrey7ansh07 <shreyguptaji007@gmail.com>
2025-01-31 18:56:56 +0530

    changes again

--- Backend/server.py
+++ Backend/server.py
@@ -120,4 +120,6 @@ def send_email():
         return json.loads(json_util.dumps(result))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.getenv("PORT", 5000))  # Use Render's provided port or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
